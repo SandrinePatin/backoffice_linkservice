a856320aa6e7da912b7fc06508e610f6953d80df
Sandrine Patin <yunie@MacBook-Pro-de-Sandrine.local>
2020-05-29 14:27:52 +0200

    create settings interface

--- src/home/Controller.java
+++ src/home/Controller.java
@@ -1,20 +1,27 @@
 package home;
 
+import Classes.API;
+import com.google.gson.Gson;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import Classes.User;
 
 import java.io.IOException;
+import java.lang.management.GarbageCollectorMXBean;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    private User userConnected;
 
     @FXML
     private VBox pnItems = null;
@@ -22,37 +29,50 @@ public class Controller implements Initializable {
     private Button btnOverview;
 
     @FXML
-    private Button btnOrders;
+    private Button btnTickets;
 
     @FXML
-    private Button btnCustomers;
+    private Button btnUsers;
 
     @FXML
-    private Button btnMenus;
-
-    @FXML
-    private Button btnPackages;
+    private Button btnForum;
 
     @FXML
     private Button btnSettings;
 
     @FXML
+    private Button btnServices;
+
+    @FXML
     private Button btnSignout;
 
     @FXML
-    private Pane pnlCustomer;
+    private Label userNameField;
 
     @FXML
-    private Pane pnlOrders;
+    private Pane pnlUsers;
+
+    @FXML
+    private Pane pnlTickets;
 
     @FXML
     private Pane pnlOverview;
 
     @FXML
-    private Pane pnlMenus;
+    private Pane pnlForum;
+
+    @FXML
+    private Pane pnlSettings;
+
+    @FXML
+    private Pane pnlServices;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        pnlOverview.setStyle("-fx-background-color : #E2E2E2");
+        pnlOverview.toFront();
+
         Node[] nodes = new Node[10];
         for (int i = 0; i < nodes.length; i++) {
             try {
@@ -76,28 +96,39 @@ public class Controller implements Initializable {
 
     }
 
-    public void initData(User sessionUser) {
-        System.out.println(sessionUser.getId());;
+    public void initData(User sessionUser) throws IOException, InterruptedException {
+        userConnected = sessionUser;
+
+        String fullName = userConnected.getName() + ' ' + userConnected.getSurname();
+        userNameField.setText(fullName);
+
     }
 
 
     public void handleClicks(ActionEvent actionEvent) {
-        if (actionEvent.getSource() == btnCustomers) {
-            pnlCustomer.setStyle("-fx-background-color : #1620A1");
-            pnlCustomer.toFront();
+        if (actionEvent.getSource() == btnUsers) {
+            pnlUsers.setStyle("-fx-background-color : #E2E2E2");
+            pnlUsers.toFront();
         }
-        if (actionEvent.getSource() == btnMenus) {
-            pnlMenus.setStyle("-fx-background-color : #53639F");
-            pnlMenus.toFront();
+        if (actionEvent.getSource() == btnForum) {
+            pnlForum.setStyle("-fx-background-color : #E2E2E2");
+            pnlForum.toFront();
         }
         if (actionEvent.getSource() == btnOverview) {
-            pnlOverview.setStyle("-fx-background-color : #02030A");
+            pnlOverview.setStyle("-fx-background-color : #E2E2E2");
             pnlOverview.toFront();
         }
-        if(actionEvent.getSource()==btnOrders)
-        {
-            pnlOrders.setStyle("-fx-background-color : #464F67");
-            pnlOrders.toFront();
+        if (actionEvent.getSource() == btnTickets) {
+            pnlTickets.setStyle("-fx-background-color : #E2E2E2");
+            pnlTickets.toFront();
+        }
+        if (actionEvent.getSource() == btnSettings) {
+            pnlSettings.setStyle("-fx-background-color : #E2E2E2");
+            pnlSettings.toFront();
+        }
+        if (actionEvent.getSource() == btnServices) {
+            pnlServices.setStyle("-fx-background-color : #E2E2E2");
+            pnlServices.toFront();
         }
     }
 }
